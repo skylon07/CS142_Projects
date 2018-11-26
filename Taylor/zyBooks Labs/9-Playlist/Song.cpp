45b9347fb239baadb68cc7e284670cb7e9ef02de
Taylor Brown <taylor@davtar.org>
2018-11-25 18:02:19 -0700

    Add completed main lab 9 - playlists

--- Taylor/zyBooks Labs/9-Playlist/Song.cpp
+++ Taylor/zyBooks Labs/9-Playlist/Song.cpp
@@ -1,1 +1,46 @@
 #include "Song.h"
+
+// default constructor does nothing
+Song::Song() { }
+
+// constructor that sets name and first line
+Song::Song(std::string songName, std::string firstLine) {
+    SetName(songName);
+    SetLine(firstLine);
+}
+
+
+// getters for members
+std::string Song::GetName() { 
+    return songName;
+}
+
+std::string Song::GetLine() {
+    return firstLine;
+}
+
+int Song::GetNumPlays() {
+    return numPlays;
+}
+
+
+// setters for variables
+void Song::SetName(std::string newName) {
+    songName = newName;
+}
+
+void Song::SetLine(std::string newFirstLine) {
+    firstLine = newFirstLine;
+}
+
+void Song::SetNumPlays(int numTimesPlayed) {
+    numPlays = numTimesPlayed;
+}
+
+
+// function that "plays" the song
+// - prints first line and increments play count
+void Song::PlaySong() {
+    std::cout << GetLine() << std::endl;
+    SetNumPlays(GetNumPlays() + 1); // incrememt by 1
+}
--- Taylor/zyBooks Labs/9-Playlist/Song.h
+++ Taylor/zyBooks Labs/9-Playlist/Song.h
@@ -1,4 +1,37 @@
 #ifndef SONG_H
 #define SONG_H
 
+#include <string>
+#include <iostream>
+
+
+class Song {
+public:
+    // constructors
+    Song(); // default
+    Song(std::string songName, std::string firstLine);
+
+    // getters for variables
+    std::string GetName();
+    std::string GetLine();
+    int GetNumPlays();
+
+    // setters for variables
+    void SetName(std::string newName);
+    void SetLine(std::string newFirstLine);
+
+    // function that "plays" the song (prints the first line, increments play count)
+    void PlaySong();
+
+private:
+    // data members
+    std::string songName = "none";
+    std::string firstLine = "none";
+    int numPlays = 0;
+
+    // setter for number of times played
+    void SetNumPlays(int numTimesPlayed);
+};
+
+
 #endif
